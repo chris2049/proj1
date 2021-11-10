7576696b3dba0db021bb17cfc4c3a330a3b2328b
Christian Cortez <ccortez5@mail.sfsu.edu>
2021-11-10 01:20:05 -0800

    update error

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,17 @@
 use thiserror::Error;
 
+use solana_program::program_error::ProgramError;
+
 #[derive(Error, Debug, Copy, Clone)]
 pub enum EscrowError {
     //invalid instruction
     #[error("Invalid Instruction")]
     InvalidInstruction,
 
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod entrypoint;
 pub mod instructions;
 pub mod error;
+pub mod processor;
