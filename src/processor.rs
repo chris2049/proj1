9537599935e43333facd69f9595d47572b711759
Christian Cortez <ccortez5@mail.sfsu.edu>
2021-11-11 21:09:02 -0800

    ye

--- src/processor.rs
+++ src/processor.rs
@@ -4,6 +4,8 @@ use solana_program::{
     program_error::ProgramError,
     msg,
     pubkey::Pubkey,
+    program_pack::{Pack, IsInitialized},
+    sysvar::{rent::Rent, Sysvar},
 };
 
 use crate::instructions::EscrowInstructions;
@@ -32,6 +34,19 @@ impl Processor {
         if *token_to_receive_account.owner != spl_token::id() {
             return Err(ProgramError::IncorrectProgramId);
         }
+
+    let escrow_account = next_account_info(account_info_iter)?;
+    let rent = &Rent::from_account_info(next_account_info(account_info_iter)?)?;
+
+    if !rent.is_exempt(escrow_account.lamports(), escrow_account.data_len()) {
+        return Err(EscrowError::NotRentExempt.into());
+    }
+
+    let mut escrow_info = Escrow::unpack_unchecked(&escrow_account.try_borrow_data()?)?;
+    if escrow_info.is_initialized() {
+        return Err(ProgramError::AccountAlreadyInitialized);
+    }
+
 
         Ok(())
     }
